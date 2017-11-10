b67ec748d702c233bd5a1810c68744667441042e
anu2202 <anvita.k@deltax.com>
2017-11-10 16:15:34 +0530

    committing UserAPIController

---
+++ Angular2MVC/Controllers/UserAPIController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using Newtonsoft.Json;
+using System.Net.Http;
+using System.Web.Http;
+using Angular2MVC.DBContext;
+
+namespace Angular2MVC.Controllers
+{
+    public class UserAPIController : BaseAPIController
+    {
+        public HttpResponseMessage Get()
+        {
+            return ToJson(UserDB.TblUsers.AsEnumerable());
+        }
+
+        public HttpResponseMessage Post([FromBody]TblUser value)
+        {
+            UserDB.TblUsers.Add(value);
+            return ToJson(UserDB.SaveChanges());
+        }
+
+        public HttpResponseMessage Put(int id, [FromBody]TblUser value)
+        {
+            UserDB.Entry(value).State = EntityState.Modified;
+            return ToJson(UserDB.SaveChanges());
+        }
+        public HttpResponseMessage Delete(int id)
+        {
+            UserDB.TblUsers.Remove(UserDB.TblUsers.FirstOrDefault(x => x.Id == id));
+            return ToJson(UserDB.SaveChanges());
+        }
+    }
+}
